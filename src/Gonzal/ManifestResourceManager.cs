2067f15a79986372532ce070b8fb9f969e31ca09
Al Gonzalez <al.gonzo@outlook.com>
2021-03-04 20:36:24 -0500

    update file license headers to fit in 80 cols

--- src/Gonzal/ManifestResourceManager.cs
+++ src/Gonzal/ManifestResourceManager.cs
@@ -1,5 +1,6 @@
 // Copyright 2020 Alberto Gonzalez. All rights reserved.
-// Licensed under the MIT License. See LICENSE.txt file in the project root for full license information.
+// Licensed under the MIT License.
+// See LICENSE.txt file in the project root for full license information.
 
 using System.IO;
 using System.Reflection;
